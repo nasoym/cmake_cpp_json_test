f5debb767459a6fac2708a11bcdaf70ae3fa462f
Sinan Goo <sinan.goo@hypoport.de>
2019-03-04 13:12:24 +0100

    commit message

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstring> 
+#include <deque>
 
 #include <chrono>
 #include <thread>
@@ -14,8 +15,6 @@
 #include <stdio.h>
 #include <sys/poll.h>
 
-
-
 #include <nlohmann/json.hpp>
 
 using namespace std;
@@ -23,160 +22,87 @@ using json = nlohmann::json;
  
 int main(int argc, char *argv[]){
 
+  // // create an empty structure (null)
+  // json j;
+  //
+  // // add a number that is stored as double (note the implicit conversion of j to an object)
+  // j["pi"] = 3.141;
+  //
+  // // add a Boolean that is stored as bool
+  // j["happy"] = true;
+  //
+  // // add a string that is stored as std::string
+  // j["name"] = "Niels";
+  //
+  //
+  //   // parse explicitly
+  //   auto j3 = json::parse("{ \"happy\": true, \"pi\": 3.141 }");
+  //
+  //  
+  //    std::cout << j.dump() << std::endl;
+  //    std::cout << j3.dump() << std::endl;
 
-
-// // create an empty structure (null)
-// json j;
-//
-// // add a number that is stored as double (note the implicit conversion of j to an object)
-// j["pi"] = 3.141;
-//
-// // add a Boolean that is stored as bool
-// j["happy"] = true;
-//
-// // add a string that is stored as std::string
-// j["name"] = "Niels";
-//
-//
-//   // parse explicitly
-//   auto j3 = json::parse("{ \"happy\": true, \"pi\": 3.141 }");
-//
-//  
-//    std::cout << j.dump() << std::endl;
-//    std::cout << j3.dump() << std::endl;
-
-
-    // while(true) {
-    //   for (std::string line; std::getline(std::cin, line);) {
-    //       std::cout << "---" << line << "---" << std::endl;
-    //   }
-    //
-    //   std::cout << "- before sleep" << std::endl;
-    //   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    //   std::cout << "- after sleep" << std::endl;
-    //
-    // }
-
-   // cout << cin.rdbuf()->in_avail() << endl;
-
-  FILE *pFile ;
-  int i ;
-  pFile = stdin ;
-
-  fd_set readfds;
-  FD_ZERO(&readfds);
-
-  struct timeval timeout;
-  timeout.tv_sec = 0;
-  timeout.tv_usec = 0;
-
-  char message[50];
-  // char c1[1];
   char c1[1024];
+  string line;
+  deque<string> lines;
 
-  string str;
-  size_t nbytes;
+
   ssize_t bytes_read;
 
   struct pollfd fds;
   int ret;
   fds.fd = 0; /* this is STDIN */
   fds.events = POLLIN;
+
   char newline = '\n';
+  json json_line;
 
   while(1) {
-    // FD_SET(STDIN_FILENO, &readfds);
-    //
-    // if (select(1, &readfds, NULL, NULL, &timeout))
-    // {
-    //   // for (std::string line; std::getline(std::cin, line);) {
-    //   //     std::cout << "---" << line << "---" << std::endl;
-    //   // }
-    //     cout << "data in stdin read one line:" << endl; 
-    //     // scanf("%s", message); 
-    //     // printf("Message: %s\n", message);
-    //
-    //     // getline(cin, str); 
-    //     // cout << str << " : newline" << endl; 
-    //
-    //     nbytes = sizeof(message);
-    //     int fd = 0;
-    //     bytes_read = read(fd, message, nbytes);
-    //     printf("Message: %s\n", message);
-    //
-    //
-    // }
-    //
-
     ret = poll(&fds, 1, 0);
     if(ret == 1) {
-      printf("Yep: ");
-      // i = (int) sizeof (stdin) ;
-      // printf("%d bytes in stdin ", i) ;
-      // getline(cin, str); 
-      // cout << str << " : newline" << endl; 
-
+      // printf("Yep: ");
       // bytes_read = read(&fds, message, nbytes);
-      // bytes_read = read(0, message, nbytes);
-      nbytes = sizeof(c1);
-      bytes_read = read(0, c1, nbytes);
-      // if (bytes_read == 0 ) {
-      //   printf("EOF");
-      //   printf(" %d", byte_count);
-      //   break;
-      // }
-      printf("(%zd)", bytes_read);
+      bytes_read = read(0, c1, sizeof(c1));
+      // printf("(%zd) ", bytes_read);
       c1[bytes_read] = 0;
-      printf("%s", c1);
-      // int byte_count = 0;
-      // while(1) {
-      //   nbytes = sizeof(c1);
-      //   bytes_read = read(0, c1, nbytes);
-      //   if (bytes_read == 0 ) {
-      //     printf("EOF");
-      //     printf(" %d", byte_count);
-      //     break;
-      //   }
-      //   printf("(%zd)", bytes_read);
-      //   printf("%s", c1);
-      //   // byte_count += 1;
-      //   // if (newline == c1[0]) {
-      //   //   printf("EOL");
-      //   //   printf(" %d", byte_count);
-      //   //   break;
-      //   // } else {
-      //   //   printf("%s", c1);
-      //   // }
-      // }
+      // printf("%s ", c1);
+      line.append(c1);
+      if ((bytes_read > 0 ) && (newline == c1[bytes_read-1])) {
+        // printf("EOL");
+        // std::cout << '\n' << line << '\n';
+        lines.push_back(line);
+        line.clear();
 
-      // bytes_read = scanf("%s", message); 
-      // printf(" %zd: %s", bytes_read, message);
-
-      // std::cout << std::hex << c1 ;
-
-      //
-      // string line;
-      // char c;
-      // while( newline != (c = readchar(&fds)) ) {
-      //   // printf("%s", c);
-      //  // line.append(c);
-      // }
-      //
-      // cout <<  line << " : newline" << endl; 
-      // int c;
-      // c = getchar();
-      //
-      // putchar(c);
-
+      }
     } else if(ret == 0) {
-      printf("No");
+    //   printf("No");
     } else {
       printf("Error");
     }
-    printf("\n");
+
+    while (!lines.empty())
+    {
+      // std::cout << " -:" << lines.front() << ":- \n";
+      json_line = json::parse(lines.front());
+      lines.pop_front();
+      // std::cout << " -:" << json_line.dump() << ":- \n";
+      if (json_line.find("command") != json_line.end()) {
+        std::cout << "command: " << json_line["command"].get<string>() << "\n";
+        // there is an entry with key "foo"
+
+        if (json_line["command"].get<string>().compare("abc") == 0) {
+          std::cout << "ABC" << "\n";
+        }
+
+      }
+    }
+
+    // printf("\n");
     sleep(1);
+    //   std::cout << "- before sleep" << std::endl;
+    //   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    //   std::cout << "- after sleep" << std::endl;
   }
   return 0;
 }
-
 
